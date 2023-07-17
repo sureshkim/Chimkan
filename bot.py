a337f6f0b363ed0321f41fea8d9e41e88480b1d2
SURESH KIM <136443686+sureshkim@users.noreply.github.com>
2023-07-17 18:51:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
-from telegram import Update, ParseMode
+import threading
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Set up logging
@@ -9,13 +10,56 @@ logger = logging.getLogger(__name__)
 # Your Telegram bot token here
 TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
 
+# List of authorized user IDs (replace with your own user IDs)
+AUTHORIZED_USERS = [123456789, 987654321]
+
+# Variable to store the message to be sent
+message_to_send = None
+
+# Variable to check if periodic sending should continue
+send_periodically = False
+
+# Function to send the message periodically
+def send_message_periodically(context: CallbackContext):
+    global message_to_send, send_periodically
+    if send_periodically and message_to_send:
+        for chat in context.bot.get_chat_members_count():
+            if chat['type'] == 'group':
+                context.bot.send_message(chat_id=chat['id'], text=message_to_send)
+
+        context.job_queue.run_once(send_message_periodically, 300)  # 300 seconds = 5 minutes
+
 # Handler for /start command
 def start(update: Update, context: CallbackContext):
     update.message.reply_text("Hello! I am your Telegram bot. Send me any message, and I'll echo it back to you.")
 
-# Handler for echoing messages
+# Handler for /forwardall command (ask the user to send a message to be sent periodically)
+def forward_all(update: Update, context: CallbackContext):
+    global message_to_send, send_periodically
+    if update.effective_user.id in AUTHORIZED_USERS:
+        update.message.reply_text("Please send the message that you want to send to all group chats every 5 minutes. Use /stop to stop the periodic sending.")
+        message_to_send = update.message.text
+        send_periodically = True
+        # Start the periodic sending
+        context.job_queue.run_once(send_message_periodically, 0)  # Start immediately
+    else:
+        update.message.reply_text("You are not authorized to use this command.")
+
+# Handler for /stop command (stop the periodic sending)
+def stop_periodic_sending(update: Update, context: CallbackContext):
+    global send_periodically
+    if update.effective_user.id in AUTHORIZED_USERS:
+        update.message.reply_text("Periodic sending has been stopped.")
+        send_periodically = False
+    else:
+        update.message.reply_text("You are not authorized to use this command.")
+
+# Handler for echoing messages (restricted to authorized users)
 def echo(update: Update, context: CallbackContext):
-    update.message.reply_text(update.message.text)
+    if update.effective_user.id in AUTHORIZED_USERS:
+        update.message.reply_text(update.message.text)
+    else:
+        update.message.reply_text("You are not authorized to use this bot.")
 
 # Error handler
 def error_handler(update: Update, context: CallbackContext):
@@ -28,9 +72,11 @@ def main():
 
     # Command handlers
     dp.add_handler(CommandHandler("start", start))
-
-    # Message handler to echo messages
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dp.add_handler(CommandHandler("forwardall", forward_all))
+    dp.add_handler(CommandHandler("stop", stop_periodic_sending))
+    
+    # Message handler to echo messages (restricted to authorized users)
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo), group=1)
 
     # Error handler
     dp.add_error_handler(error_handler)
@@ -42,4 +88,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-          
